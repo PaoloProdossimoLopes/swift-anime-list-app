04b55436e011bcebe72211d8c3ba45c96a9db84a
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2022-02-27 23:05:23 -0300

    DOC (APP): CREATE APP FILE

---
+++ AnimeList/AnimeListApp/AnimeListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimeListApp
+//
+//  Created by Paolo Prodossimo Lopes on 27/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
